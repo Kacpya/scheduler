21823963906faf11f1af728d88a40b0dd2acfd21
kate <150615889+cbxlnx@users.noreply.github.com>
2024-02-14 14:25:30 +0000

    Update Sidebar.vue
    some improvements such a dropdown list

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,20 +3,25 @@
     <div class="logo-container">
       <a href="https://imgbox.com/0bBpBZDc" target="_blank"><img src="https://thumbs2.imgbox.com/1a/8b/0bBpBZDc_t.png" alt="Logo"/></a>
     </div>
-
-    <aside class="sidebar">
-      <!-- Sidebar content goes here -->
+    <nav class="menu">
       <ul>
-        <nav>
-          <a href="#"> <router-link to="/">Home</router-link></a>
-          <a href="#"> <router-link to="/about-us">About-Us</router-link> </a>
-          <a href="#" class="login"> <router-link to="/login">Login</router-link></a>
-          <div class="animation start-home"></div>
-
-        </nav>
+        <li><a href="#"><router-link to="/">Home</router-link></a></li>
+        <li><a href="#"><router-link to="/about-us">About-Us</router-link></a></li>
+        <li>
+          <a href="#"><router-link to="/login">My account</router-link></a>
+          <ul>
+            <li><a href="#">My timetable</a></li>
+            <li><a href="#">Settings</a></li>
+             <li><a href="#">Log out</a></li>
+          </ul>
+        </li>
       </ul>
-    </aside>
-  </div>
+      <ul class="right-menu">
+        <li><a href="#" class="login-btn"><router-link to="/login">Log in</router-link></a></li>
+        <li><a href="#"><router-link to="/login">Sign up</router-link></a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
