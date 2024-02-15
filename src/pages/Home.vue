184bc94536d7f7f289ab10f5ac861bf73e2d28b9
kate <150615889+cbxlnx@users.noreply.github.com>
2024-02-15 16:07:09 +0000

    Update Home.vue
    !

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from '../components/Header.vue'
 import MyTable from '../components/MyTable.vue'
+import Calendar from '../components/Calendar.vue'
 import Logo from "@/components/Logo.vue";
 
 </script>
@@ -8,5 +9,6 @@ import Logo from "@/components/Logo.vue";
 <template>
   <logo/>
   <Header/>
+  <Calendar/>
   <MyTable/>
 </template>
