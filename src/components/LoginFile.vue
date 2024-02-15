837c018ee1874473f7e4e82d7e2c9a43f0c38cd9
Mira <150616182+poppybun@users.noreply.github.com>
2024-02-15 15:26:08 +0000

    Update LoginFile.vue

--- src/components/LoginFile.vue
+++ src/components/LoginFile.vue
@@ -6,50 +6,16 @@
     <h2>Login</h2>
     <form @submit.prevent="login">
       <label for="username">Username:</label>
-      <input type="text" id="username" v-model="username" required />
+      <input class="login_Input" type="text" id="username" v-model="username" required />
 
-      <label for="password">Password:</label>
-      <input type="password" id="password" v-model="password" required />
+      <label class="login_Label" for="password">Password:</label>
+      <input class="login_Input" type="password" id="password" v-model="password" required />
 
-      <button type="submit">Login</button>
+      <button type="submit" class="loginFileButton">Login</button>
     </form>
 
-    <div v-if="error" class="error-message">{{ error }}</div>
+    <div v-if="error" class="login_error-message">{{ error }}</div>
   </div>
 </template>
 
-<style scoped>
-.login-container {
-  max-width: 300px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-input {
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 10px;
-}
-
-button {
-  background-color: #4caf50;
-  color: white;
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.error-message {
-  color: #ff0000;
-  margin-top: 10px;
-}
-
-</style>
+<style scoped></style>
