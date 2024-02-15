ac4fcbe58d58328cacc9ce2f735f0c2835c0884c
Mira <150616182+poppybun@users.noreply.github.com>
2024-02-15 12:15:59 +0000

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,31 +1,11 @@
+<script setup>
+
+</script>
+
 <template>
-  <div class="menu-container">
-    <div class="logo-container">
-      <a href="https://imgbox.com/0bBpBZDc" target="_blank"><img src="https://thumbs2.imgbox.com/1a/8b/0bBpBZDc_t.png" alt="Logo"/></a>
-    </div>
-    <nav class="menu">
-      <ul>
-        <li><a href="#"><router-link to="/">Home</router-link></a></li>
-        <li><a href="#"><router-link to="/about-us">About-Us</router-link></a></li>
-        <li>
-          <a href="#"><router-link to="/login">My account</router-link></a>
-          <ul>
-            <li><a href="#">My timetable</a></li>
-            <li><a href="#">Settings</a></li>
-             <li><a href="#">Log out</a></li>
-          </ul>
-        </li>
-      </ul>
-      <ul class="right-menu">
-        <li><a href="#" class="login-btn"><router-link to="/login">Log in</router-link></a></li>
-        <li><a href="#"><router-link to="/login">Sign up</router-link></a></li>
-      </ul>
-    </nav>
-  </header>
+
 </template>
 
-<script>
-export default {
-  name: 'Sidebar',
-};
-</script>
+<style scoped>
+
+</style>
