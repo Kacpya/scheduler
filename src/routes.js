821aa09f60ee3dd902eb41d927144b06a87fdb91
Mira <150616182+poppybun@users.noreply.github.com>
2024-02-15 15:22:15 +0000

    Update routes.js
    new routes added

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
-function loadPage (component) {
-// '@' is aliased to src/components
+function loadPage(component) {
+    // '@' is aliased to src/components
     return () => import(/* webpackChunkName: "[request]" */
-        `@/pages/${component}.vue`)}
+        `@/pages/${component}.vue`)
+}
 export default [
     { path: '/', component: loadPage('Home') },
     { path: '/login', component: loadPage('Login') },
-    { path: '/about-us', component: loadPage('Aboutus') }
+    { path: '/about-us', component: loadPage('Aboutus') },
+    { path: '/settings', component: loadPage('Settings') },
+    { path: '/myTable', component: loadPage('Home') },
+    { path: '/logOut', component: loadPage('LogOut') }
 ]
