a2d7e49f22e1e59e3e878d04753666212f16d7fd
kate <150615889+cbxlnx@users.noreply.github.com>
2024-02-15 16:09:37 +0000

    Update LoginFile.vue
    new login page using bootstrap

--- src/components/LoginFile.vue
+++ src/components/LoginFile.vue
@@ -2,20 +2,42 @@
 </script>
 
 <template>
-  <div class="login-container">
-    <h2>Login</h2>
-    <form @submit.prevent="login">
-      <label for="username">Username:</label>
-      <input class="login_Input" type="text" id="username" v-model="username" required />
-
-      <label class="login_Label" for="password">Password:</label>
-      <input class="login_Input" type="password" id="password" v-model="password" required />
-
-      <button type="submit" class="loginFileButton">Login</button>
-    </form>
-
-    <div v-if="error" class="login_error-message">{{ error }}</div>
+   <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+ 
+</head>
+<body>    
+  <div class="site-content">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col">
+          <div class="form-container">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+</body>
+
+ 
 </template>
 
-<style scoped></style>
+
