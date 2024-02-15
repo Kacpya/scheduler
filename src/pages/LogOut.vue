57443888abc1f6bace6333516973525f4d93af66
Mira <150616182+poppybun@users.noreply.github.com>
2024-02-15 15:23:08 +0000

    Add files via upload

---
+++ src/pages/LogOut.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Logo from "@/components/Logo.vue";
+</script>
+
+<template>
+    <logo />
+    <Header />
+</template>
+
+<style scoped></style>
---
+++ src/pages/Settings.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Logo from "@/components/Logo.vue";
+</script>
+
+<template>
+    <logo />
+    <Header />
+    <button>Settings</button>
+    <div class="settings_container">
+        <div class="site-content">
+            <h2>Change language:</h2>
+            <form>
+                <input type="radio" id="english" name="app_language" value="English">
+                <label for="english">English</label>
+                <br>
+                <input type="radio" id="ukrainian" name="app_language" value="Ukrainian">
+                <label for="ukrainian">Ukrainian</label>
+            </form>
+            <button>Save</button>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
