19b455d31a951716a9c53a7da9f60aff0594b781
Mira <150616182+poppybun@users.noreply.github.com>
2024-03-11 12:02:08 +0000

    Update routes.js
    authentication, new routes

--- src/routes.js
+++ src/routes.js
@@ -9,5 +9,26 @@ export default [
     { path: '/about-us', component: loadPage('Aboutus') },
     { path: '/settings', component: loadPage('Settings') },
     { path: '/myTable', component: loadPage('Home') },
-    { path: '/logOut', component: loadPage('LogOut') }
+    { path: '/logOut', component: loadPage('LogOut') },
+    { path: '/register', component: loadPage('Registration') },
+    { path: '/secure', component: loadPage('Secure'), beforeEnter : isAuth }
 ]
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "./api/firebase"; 
+function isAuth(to, from, next) {
+    console.log("Checking auth");
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log(user);
+            // User is signed in so continue to desired page
+            return next();
+            // ...        
+        } else {
+            // User is signed out
+            // Send them back to the home page or maybe the login page
+            return next({ path: '/' });
+        }
+    });
+}
